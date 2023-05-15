728790a57705ebf2870be2e3c5a99fd76e5b092b
AhmadOunabi <ahmad_ounabi@yahoo.com>
2023-05-15 18:16:11 +0200

    Add

--- app1/views.py
+++ app1/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from app1.models import Topic, Comment
 
 # Create your views here.
 
 def index(request):
-    my_dic={'insert_me':"Hello Iam from views.py"}
-    return render(request, 'index.html', context=my_dic)
+    topic_list=Topic.objects.all()
+    topic_dic={'topics':topic_list}
+    return render(request, 'index.html', context=topic_dic)
